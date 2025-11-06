f1797c643aa108633602292fe732d2c730b5cfdd
Mia Rose Winter <mia.winter@winter-software.com>
2025-11-06 12:49:10 +0100

    prettier

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -1,8 +1,8 @@
-import { bootstrapApplication } from '@angular/platform-browser';
-import { appConfig } from './app/app.config';
-import { App } from './app/app';
-import "@fontsource/atkinson-hyperlegible"
-import "@fontsource/atkinson-hyperlegible-mono"
-import "@fontsource/nunito-sans"
+import { bootstrapApplication } from "@angular/platform-browser";
+import { appConfig } from "./app/app.config";
+import { App } from "./app/app";
+import "@fontsource/atkinson-hyperlegible";
+import "@fontsource/atkinson-hyperlegible-mono";
+import "@fontsource/nunito-sans";
 
 bootstrapApplication(App, appConfig).catch((err) => console.error(err));
